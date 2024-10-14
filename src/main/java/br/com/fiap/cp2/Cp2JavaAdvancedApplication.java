d69423dca547ceecbc5bdcee60ffb8b890fa9a7b
Lívia Mariana Lopes <liviamariana2004@gmail.com>
2024-10-14 20:57:59 -0300

    :sparkles: feat: Classes do Spring Security

--- src/main/java/br/com/fiap/cp2/Cp2JavaAdvancedApplication.java
+++ src/main/java/br/com/fiap/cp2/Cp2JavaAdvancedApplication.java
@@ -1,9 +1,13 @@
 package br.com.fiap.cp2;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info =
+@Info(title = "API de diplomas", version = "0.0.1", description = "API de diplomas para a realização do CP2"))
 public class Cp2JavaAdvancedApplication {
 
 	public static void main(String[] args) {
